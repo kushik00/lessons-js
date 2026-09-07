0e8fd0fe6cfcd31fc68f4988fa5ba9aa6d03a549
Lada Shcherbakova <146574580+kushik00@users.noreply.github.com>
2026-09-07 11:12:04 +0300

    Add files via upload

---
+++ school_js/para_01/script.js
@@ -0,0 +1,72 @@
+//42;
+//4.5; - number
+
+// 'use str'; - string
+
+//true(1); false(0,NoN,null,undefined); 
+
+//var, let, const
+
+// var a;
+// a = 10;
+// console.log(a);
+
+// var b = 20;
+// var c = 12, d =12;
+
+// let allow = '32';
+
+// a = 13;
+// let a;
+// console.log(a);
+
+// const a = 4;
+
+// let a =15, b = 26;
+// console.log(a+b);
+// console.log(b-a);
+// console.log(a*b);
+// console.log(b ** a);
+// console.log(a/b);
+// console.log(b%a);
+// console.log(b/a);
+
+// a = a +=1;
+// a++;
+// a--;
+// ++a;
+
+// let a = 2, b;
+// // b = a++;
+// b = ++a;
+// console.log(b);
+
+// let a = '4'; 
+// let b = 2;
+
+// console.log(a - b);
+// console.log(a + b);
+// console.log((a-0) + b);
+
+// a = Number(a)
+// console.log(a + b);
+
+
+// інтерполяція - формат рядкового значення 
+// let name = 'Lada'
+// let age = 16
+
+// console.log(`привіт ${name} ! тобі ${age} років`);
+
+// console.info('hello');
+// console.log('hello');
+// console.warn('hello');
+// console.error('hello');
+
+// alert('hello');
+// let name = prompt('як тебе звати')
+// alert(name);
+
+let a = Number(prompt('number1?'))
+let b = Number(prompt('number2?'))
+console.log(a+b);
